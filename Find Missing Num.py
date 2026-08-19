72eb25baa0cf58757e8fb3686b4102628a25d549
swapnaamara <swapnaamara59@gmail.com>
2026-08-19 14:21:30 +0530

    Rename file to Find Missing Num.py

--- Find Missing Num.py
+++ Find Missing Num.py
@@ -1,13 +1,11 @@
 numbers = [1, 2, 3, 5, 6]
 
 n = 6
-
 expected_sum = n * (n + 1) // 2
 actual_sum = 0
 
 for num in numbers:
     actual_sum += num
-
 missing = expected_sum - actual_sum
 
 print("Missing number:", missing)
