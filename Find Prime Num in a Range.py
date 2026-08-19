117529db8ab8a652e0c7c07ad16ce7669f6fe47c
swapnaamara <swapnaamara59@gmail.com>
2026-08-19 14:22:12 +0530

    Update and rename Find Prime Numbers in a Range.py to Find Prime Num in a Range.py

--- Find Prime Num in a Range.py
+++ Find Prime Num in a Range.py
@@ -2,14 +2,12 @@ start = 10
 end = 30
 
 for num in range(start, end + 1):
-
     if num < 2:
         continue
 
     is_prime = True
 
     for i in range(2, int(num ** 0.5) + 1):
-
         if num % i == 0:
             is_prime = False
             break
