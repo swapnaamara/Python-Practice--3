53e6176e69b557cf73f8852402811bf105b05e3c
swapnaamara <swapnaamara59@gmail.com>
2026-09-23 19:52:47 +0530

    Refactor prime number finder code formatting

--- Find Prime Num in a Range.py
+++ Find Prime Num in a Range.py
@@ -8,6 +8,7 @@ for num in range(start, end + 1):
     is_prime = True
 
     for i in range(2, int(num ** 0.5) + 1):
+        
         if num % i == 0:
             is_prime = False
             break
