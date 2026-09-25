abf61cbdb17c4189e685192e7f05ca86647fb66d
swapnaamara <swapnaamara59@gmail.com>
2026-09-25 19:09:13 +0530

    Fix formatting issues in Word Frequency.py

--- Word Frequency.py
+++ Word Frequency.py
@@ -1,11 +1,9 @@
 sentence = "python is easy and python is powerful"
-
 words = sentence.split()
-
 frequency = {}
 
 for word in words:
-
+    
     if word in frequency:
         frequency[word] += 1
     else:
